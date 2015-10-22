35d1c76532dfe29c05f033b0b5c47a827afe24e3
Satu <s.j.hakkinen@gmail.com>
2015-10-22 21:13:47 +0300

    Ilmoitusnäkymän hahmottelua

---
+++ myscripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+//    Listeneri hakuehtomodalille
+    $('#refineButton').click(function () {
+        $('#refineModal').modal('show');
+    });
+
+    $('#refineLocation')
+  .dropdown()
+;
+
+});

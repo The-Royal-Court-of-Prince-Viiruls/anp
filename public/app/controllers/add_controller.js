0ea7bb549b1446f96508cf493c0c8e65ef9dd943
satuhakk <s.j.hakkinen@gmail.com>
2015-10-27 16:52:03 +0200

    Ilmolomake validoitu ja toimii kivasti

--- public/app/controllers/add_controller.js
+++ public/app/controllers/add_controller.js
@@ -10,19 +10,6 @@ $scope.newShippingMethods = {
 }
 
   $scope.addPost = function() {
-    PostService.addPost({
-      type: $scope.newType,
-      item: $scope.newItem,
-      category: $scope.newCategory,
-      description: $scope.newDescription,
-      condition: $scope.newCondition,
-      location: $scope.newLocation,
-      shipping: $scope.newShippingMethods,
-      contact: {
-        gsm: $scope.newGSM,
-        email: $scope.newEmail
-      }
-    });
     FirebaseService.addPost({
       type: $scope.newType,
       item: $scope.newItem,

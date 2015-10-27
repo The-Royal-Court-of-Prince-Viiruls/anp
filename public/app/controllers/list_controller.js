de4bf4c61f5375c58b9d7158b94d0064ad5df916
satuhakk <s.j.hakkinen@gmail.com>
2015-10-27 20:10:16 +0200

    Ilmoitusten listaus työn alla, hakuehtolomakkeen vaihtoehdot löytyy

--- public/app/controllers/list_controller.js
+++ public/app/controllers/list_controller.js
@@ -2,4 +2,14 @@ IlmoitusApp.controller('ListController', function ($scope, FirebaseService) {
 
   $scope.posts = FirebaseService.getPosts();
 
+// Open the search refining modal
+  $scope.refineModal = function() {
+    $('#refineModal').modal('show');
+  };
+
+  $scope.accordion = function() {
+    $('.ui.accordion')
+  .accordion();
+  };
+
 })

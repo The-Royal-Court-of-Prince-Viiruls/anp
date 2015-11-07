7731d07ce0dcf17505ae62f72adf54bb6efd3729
satuhakk <s.j.hakkinen@gmail.com>
2015-11-07 15:16:43 +0200

    Kommentti-ikonin ruukkausta

--- public/app/controllers/user_controller.js
+++ public/app/controllers/user_controller.js
@@ -1,47 +1,49 @@
 IlmoitusApp.controller('UserController', function ($scope, PostService, UserService, $rootScope) {
-$scope.usersPosts = [];
-$scope.postId = "";
-$scope.postsComments = [];
+  $scope.usersPosts = [];
+  $scope.postId = "";
+  $scope.postsComments = [];
 
-UserService.listUsersPosts($rootScope.user.id).then(function(d) {
+  UserService.listUsersPosts($rootScope.user.id).then(function(d) {
     $scope.usersPosts = d;
- });
+  });
 
- $scope.unansweredQuestions = function(posti) {
-   var i = posti.questions.length;
-   while (i--) {
-     if (!posti.questions[i].contains(reply)) {
-       console.log('tru')
-       return true;
-     }
-   }
-   return false
- }
+  $scope.unansweredQuestions = function(posti) {
+    if (typeof posti.questions === 'undefined') {
+      return false;
+    }
+    var i = posti.questions.length;
+    while (i--) {
+      if (typeof posti.questions[i].reply === 'undefined') {
+        return true;
+      }
+    }
+    return false
+  }
 
- $scope.openCommentModal = function(posti) {
-   $scope.postsComments = posti.questions;
-   $scope.postId = posti._id;
-   $('#commentModal')
-   .modal('show')
-   ;
- }
+  $scope.openCommentModal = function(posti) {
+    $scope.postsComments = posti.questions;
+    $scope.postId = posti._id;
+    $('#commentModal')
+    .modal('show')
+    ;
+  }
 
- $scope.showAnswerBox = function(event) {
-   event.currentTarget.parentElement.getElementsByClassName("ui fluid action input")[0].style.display = '';
- }
+  $scope.showAnswerBox = function(event) {
+    event.currentTarget.parentElement.getElementsByClassName("ui fluid action input")[0].style.display = '';
+  }
 
- $scope.removePost = function(post) {
-   UserService.removePost(post._id, post.user);
- }
+  $scope.removePost = function(post) {
+    UserService.removePost(post._id, post.user);
+  }
 
- $scope.sendReply = function(questionId, event) {
-   var replyInfo = {
-     questionId: questionId,
-     reply: event.target.parentElement.childNodes[1].value,
-     sender: $rootScope.user.email,
-     timestamp: Date.now()
-   }
-   UserService.addReply(replyInfo,$scope.postId);
- }
+  $scope.sendReply = function(questionId, event) {
+    var replyInfo = {
+      questionId: questionId,
+      reply: event.target.parentElement.childNodes[1].value,
+      sender: $rootScope.user.email,
+      timestamp: Date.now()
+    }
+    UserService.addReply(replyInfo,$scope.postId);
+  }
 
 })
